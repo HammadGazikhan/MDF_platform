6cdfa1e80de32f6c4c0054e9b3b9d058fde4dff3
Hammad Khan <khanhammad9608@gmail.com>
2025-08-13 08:42:24 +0000

    not fix yet lookup tables menu not working

--- src/components/ui/collapsible.tsx
+++ src/components/ui/collapsible.tsx
@@ -2,23 +2,11 @@
 "use client"
 
 import * as CollapsiblePrimitive from "@radix-ui/react-collapsible"
-import { Slot } from "@radix-ui/react-slot";
-import * as React from "react";
 
 const Collapsible = CollapsiblePrimitive.Root
 
-const CollapsibleTrigger = React.forwardRef<
-  React.ElementRef<typeof CollapsiblePrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof CollapsiblePrimitive.Trigger> & {
-    asChild?: boolean;
-  }
->(({ asChild = false, ...props }, ref) => {
-  const Comp = asChild ? Slot : "button";
-  return <Comp ref={ref} {...props} />;
-});
-CollapsibleTrigger.displayName = "CollapsibleTrigger"
+const CollapsibleTrigger = CollapsiblePrimitive.Trigger
 
-
-const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent
+const CollapsibleContent = CollapsiblePrimitive.Content
 
 export { Collapsible, CollapsibleTrigger, CollapsibleContent }
